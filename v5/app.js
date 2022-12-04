17951fef86fbe2a973399dacb2cfd0bf3d32a64f
muhafash <me.muhfarash@gmail.com>
2022-12-05 06:28:30 +0700

    video 5

--- v5/app.js
+++ v5/app.js
@@ -1,33 +1,44 @@
 // Finding the Index
 let pet = "Birdy the Bird";
-// console.log(pet);
-// console.log(pet[2]);
-// console.log(pet[8]);
-// console.log(pet[4]);
-// console.log(pet[pet.length - 1]);
-// console.log(pet[pet.length - 2]);
-// console.log(pet[pet.length - 3]);
+console.log(pet); //Birdy the Bird
+//use index of array that start from 0
+console.log(pet[2]); //r
+console.log(pet[8]); //e
+console.log(pet[4]); //y
+// length properties behave like object properties and use index of array but start from 1 unlike array thats start from 0
+console.log(pet[pet.length - 1]); //d
+console.log(pet[pet.length - 2]); //r
+console.log(pet[pet.length - 3]); //i
 
-// trim method
-// let song = "    shallow      ";
-// console.log(song.trim());
+// trim method => make it trim delete an whitespace
+let shoes = "    nike      ";
+console.log(shoes); //    nike
+console.log(shoes.trim()); //nike
+shoes = "   nike vs adidas";
+console.log(shoes); //     nike vs adidas
+console.log(shoes.trim()); //nike vs adidas
 
-// indexOf method
-// console.log(pet.indexOf("y"));
+// indexOf method => use to find the index of target start from 0
+console.log(pet.indexOf("y")); //4
 
-// charAt method
-// console.log(pet.charAt("4"));
+// charAt method => find the character at index target
+console.log(pet.charAt("4")); //y
 
-// slice method
-// console.log(pet.slice(4));
-// console.log(pet.slice(4, 10));
+// slice method =>  cut a value at index target
+console.log(pet.slice(4)); //y the Bird
+// this one give an range index
+console.log(pet.slice(4, 9)); //y the
+//combine method
+console.log(pet.slice(5, 9).trim()); //the
 
-// split method
-// console.log(pet.split(" "));
+// split method  => its like take take this index target and print the rest
+console.log(pet.split(" ")); //(3) ['Birdy', 'the', 'Bird']
+console.log(pet.split("")); //(14) ['B', 'i', 'r', 'd', 'y', ' ', 't', 'h', 'e', ' ', 'B', 'i', 'r', 'd']
+console.log(pet.split("the")); //(2) ['Birdy ', ' Bird']
 
-// includes method
-// console.log(pet.includes("@"));
-// console.log(pet.includes("i"));
+// includes method => method use to check if value assigned is there
+console.log(pet.includes("@")); //false
+console.log(pet.includes("i")); //true
 
-// replace method
-// console.log(pet.replace("the", "a"));
+// replace method => method use to delete target value and change with a new value
+console.log(pet.replace("the", "a")); //Birdy a Bird
